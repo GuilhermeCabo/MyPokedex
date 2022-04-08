534c9554de00b07cc7e49fb07b34bbffa57e22c3
GuilhermeCabo <guilherme.pcabo@gmail.com>
2022-04-08 00:37:26 -0300

    feat: first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Some text</Text>
+    </View>
+  );
+};
+
+export { Home };
